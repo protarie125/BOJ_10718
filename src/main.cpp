657fc310e37c2af3e59d16f3c9947d92c9d26a2a
protarie125 <proarie@gmail.com>
2022-03-09 10:43:08 +0900

    초기 커밋.

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	for (int i = 0; i < 2; ++i) {
+		cout << "강한친구 대한육군\n";
+	}
+
+	return 0;
+}
